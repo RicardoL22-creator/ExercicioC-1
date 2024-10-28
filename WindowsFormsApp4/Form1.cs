d73a7afa690328511dbbb98cb2b2386c36be0abf
Ricardo Lopes <Ric22031999@gmail.com>
2024-10-27 21:37:38 -0300

    Primeiro projeto em C#

---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+
+        private int contador = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            contador++;
+            label1.Text = contador.ToString();
+
+        }
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (contador > 0)
+            {
+            contador--;
+            label1.Text = contador.ToString();
+}
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            contador = 0;
+            label1.Text = contador.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+       
+    }
+}
